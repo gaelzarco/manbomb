b3904fc76db8005da8fa8806674b00ffbacd6c3b
Gael Zarco <gaelxarco@icloud.com>
2024-02-29 21:17:53 -0800

    performance metrics and new 3D text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} h-screen w-full bg-black text-white`}
+        className={`${inter.className} h-screen w-full bg-[rgb(255,179,71)] text-black`}
       >
         <div>{children}</div>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { PerspectiveCamera, Html } from "@react-three/drei";
+import { PerspectiveCamera, Html, Text3D } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
 import Button from "@/components/button";
 import ManBomber from "@/components/manbomber";
@@ -10,15 +12,19 @@ export default function Home() {
   return (
     <main className="w-full h-screen cursor-default">
       <Canvas className="fixed w-full h-full m-0 p-0">
+        <Perf position="top-left" />
+        <OrbitControls makeDefault />
         <PerspectiveCamera position={[0, 0, 0]}>
-          <pointLight intensity={0.7} castShadow position={[1, 0.5, 1]} />
+          <Text3D position={[-4, 1, 0]} font="assets/Dogways.json">
+            MANBOMBER
+          </Text3D>
+          <pointLight intensity={0.2} position={[1, 0.5, 1]} />
           <ambientLight intensity={0.2} />
           <ManBomber />
         </PerspectiveCamera>
 
         <Html>
           <div className="fixed w-full h-full flex flex-col content-center items-center justify-center">
-            <h1 className="font-bold text-3xl">MANBOMB</h1>
             <div className="flex flex-row justify-center mt-20">
               <Button>Login</Button>
             </div>
